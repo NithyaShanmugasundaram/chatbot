0401ab769364c661bf4d31a7f81dbc295a0691a4
Ganesh kumar M <gunz@Ganeshs-MacBook-Pro.local>
2019-06-14 16:10:25 +0800

    Updates

---
+++ precache-manifest.042adec8678630998438ac02ffa2eb97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa959d85c9a964096a8238e91991dbb",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/chatbot/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "489645afb0bc293d1d54",
+    "url": "/chatbot/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/chatbot/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "489645afb0bc293d1d54",
+    "url": "/chatbot/static/js/main.5f03fe81.chunk.js"
+  },
+  {
+    "revision": "91a29a2feaa19ef2f64f",
+    "url": "/chatbot/static/js/runtime~main.312c33d4.js"
+  }
+]);
